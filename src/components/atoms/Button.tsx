7e5b578478dd002d1dee32e195d2291c870d8f06
anandarizkyrm <rizky.pky@gmail.com>
2023-03-07 21:15:32 +0700

    create login page

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,14 +1,39 @@
-import React from 'react'
+import { forwardRef } from 'react'
+import cn from 'clsx'
+import { Loading } from './Loading'
+import type { ComponentPropsWithRef } from 'react'
 
-type Props = {
-    text : string
+type ButtonProps = ComponentPropsWithRef<'button'> & {
+    loading?: boolean
 }
 
-const Button = (props: Props) => {
-  const { text } = props
-  return (
-    <button className=''>{text}</button>
-  )
-}
+// eslint-disable-next-line react/display-name
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+    ({ className, loading, disabled, children, ...rest }, ref) => {
+        // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+        const isDisabled = loading || disabled
 
-export default Button
+        return (
+            <button
+                className={cn(
+                    'custom-button main-tab rounded-3xl',
+                    loading &&
+                        'relative !text-transparent disabled:cursor-wait',
+                    className
+                )}
+                type="button"
+                disabled={isDisabled}
+                ref={ref}
+                {...rest}
+            >
+                {loading && (
+                    <Loading
+                        iconClassName="h-5 w-5"
+                        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                    />
+                )}
+                {children}
+            </button>
+        )
+    }
+)
